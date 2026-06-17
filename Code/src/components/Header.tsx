ece954709cc9e1f2ccf94c3ee549384d4ae87a5f
adhithmk <adhithmk26@gmail.com>
2026-06-17 13:53:48 +0530

    Update header logo to Birdlab logo image asset and path

--- Code/src/components/Header.tsx
+++ Code/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="sticky-header">
       <div className="header-content">
-        <img src="/iiser tpt.png" alt="IISER Tirupati Logo" className="header-logo" />
+        <img src="/Birdlab_logo.jpeg" alt="IISER Tirupati Logo" className="header-logo" />
         <h1 className="header-title">Bioacoustics Monitoring in Lantana Invaded Landscapes</h1>
       </div>
     </header>
